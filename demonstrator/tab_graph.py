2153f5e7bd811f07d126927f3f889615087648da
LAUBENicolas <n.laube05@gmail.com>
2022-02-26 14:53:27 +0100

    added example use of agraph

--- demonstrator/tab_graph.py
+++ demonstrator/tab_graph.py
@@ -2,8 +2,73 @@
 
 import streamlit as st
 
+# from owlready2 import default_world
+from streamlit_agraph import Config, Edge, Node, agraph  # TripleStore
+
 
 def tab_graph_view():
     """Tab graph view"""
 
     st.markdown("# Graph")
+
+    nodes = []
+    edges = []
+
+    nodes.append(
+        Node(
+            id="Spiderman",
+            label="Peter Parker",
+            size=400,
+            svg="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png",
+        )
+    )  # includes **kwargs
+    nodes.append(
+        Node(
+            id="Captain_Marvel",
+            size=400,
+            svg="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png",
+        )
+    )
+    edges.append(
+        Edge(
+            source="Captain_Marvel",
+            label="friend_of",
+            target="Spiderman",
+            type="CURVE_SMOOTH",
+        )
+    )  # includes **kwargs
+
+    config = Config(
+        width=500,
+        height=500,
+        directed=True,
+        nodeHighlightBehavior=True,
+        highlightColor="#F7A7A6",  # or "blue"
+        collapsible=True,
+        node={"labelProperty": "label"},
+        link={"labelProperty": "label", "renderLabel": True}
+        # **kwargs e.g. node_size=1000 or node_color="blue"
+    )
+
+    agraph(nodes=nodes, edges=edges, config=config)
+
+
+# graph = default_world.as_rdflib_graph()
+# store = TripleStore()
+
+# config = Config(
+#     width=500,
+#     height=500,
+#     directed=True,
+#     nodeHighlightBehavior=True,
+#     highlightColor="#F7A7A6",  # or "blue"
+#     collapsible=True,
+#     node={"labelProperty": "label"},
+#     link={"labelProperty": "label", "renderLabel": True}
+#     # **kwargs e.g. node_size=1000 or node_color="blue"
+# )
+
+# for subj, pred, obj in graph:
+#     store.add_triple(subj, pred, obj, "")
+
+# agraph(list(store.getNodes()), list(store.getEdges()), config)
