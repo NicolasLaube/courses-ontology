6a21b06994b34bf99c09d35d2864494d1d636b35
LAUBENicolas <n.laube05@gmail.com>
2022-02-28 19:19:33 +0100

    added knwoledge till country pop

--- src/instances/micro_learning/music/knowledge.py
+++ src/instances/micro_learning/music/knowledge.py
@@ -36,4 +36,40 @@ with ONTOLOGY:
     beatles_creation_in_1957 = Knowledge("BeatlesCreatedIn1957")
     beatles_last_member = Knowledge("RingoStarrLastMember")
     # caracteristics of pop
-    pop_turned_towards_lighter_themes = Knowledge("2255211")
+    pop_turned_towards_lighter_themes = Knowledge("PopTurnedToLighterThematics")
+    pop_musics_structures_are_simple = Knowledge("PopMusicStructuresAreSimple")
+    pop_music_melodies_stay_in_mind = Knowledge("PopMusicMelodiesStayInMind")
+    pop_music_short = Knowledge("PopMusicsAreShort")
+    pop_music_highlights_musicians = Knowledge("PopMusicHighlightsMusicians")
+    # bubblegum pop
+    bubblegum_pop_is_subgenre_of_pop = Knowledge("BubbleGumPopIsSubgenreOfPop")
+    bubblegum_pop_created_for_teenagers = Knowledge("BubblegumPopCreatedForTeenagers")
+    bubblegum_pop_exploited_by_brands = Knowledge("BubblegumPopExploitedBysBrands")
+    archies_sugar_is_bubblegum_pop = Knowledge("ArchiesSugarIsBubblegumPop")
+    bubblegum_pop_because_of_teens_bubblegum = Knowledge(
+        "BubblegumPopBecauseOfTeensBubblegum"
+    )
+    # baroque pop
+    pop_baroque_is_subgenre_of_pop = Knowledge("PopBaroqueIsSubgenreOfPop")
+    pop_baroque_mixes_classical_baroque_and_pop = Knowledge(
+        "PopBaroqueMixesClassicalBaroqueAndPop"
+    )
+    pop_baroque_uses_instruments_of_classical_musique = Knowledge(
+        "PopBaroqueUsesInstrumentsOfClassicalMusique"
+    )
+    wouldnt_it_be_nice_is_pop_baroque = Knowledge("WouldntItBeNiceIsPopBaroque")
+    wouldnt_it_be_nice_contains_piano = Knowledge("WouldntItBeNiceContainsPiano")
+    wouldnt_it_be_nice_from_beach_boys = Knowledge("WouldntItBeNiceFromBeachBoys")
+    # country pop
+    country_pop_is_subgenre_of_pop = Knowledge("CountryPopIsSubgenreOfPop")
+    country_pop_developed_by_members_of_country_musique_for_audience = Knowledge(
+        "PopCountryDevelopedForLargerAudienceByCountryMusicians"
+    )
+    ill_always_love_you_is_pop_country = Knowledge("IllAlwaysLoveYouIsPopCountry")
+    ill_always_love_you_from_dolly_parton = Knowledge("IllAlwaysLoveYouFromDollyParton")
+    iconic_instrument_of_country_is_harmonica = Knowledge(
+        "IconicInstrumentOfCountryIsHarmonica"
+    )
+    contemporary_artist_of_pop_country_Taylor_S = Knowledge(
+        "TaylorsSwiftContemporaryArtistOfPopCountry"
+    )
